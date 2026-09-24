8a368c7ac615e181d4b6723555e967eebd4e4011
anonymous-alpaca86 <ngochandp186@gmail.com>
2026-09-24 13:12:34 -0400

    Add pages for more detail of each pet

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .serializers import UserProfileSerializer,UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 # Create your views here.
 
 class ProfileView(generics.RetrieveUpdateAPIView):
@@ -12,3 +13,4 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 class SignUpView(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class=UserSerializer
+    permission_classes=[AllowAny]
